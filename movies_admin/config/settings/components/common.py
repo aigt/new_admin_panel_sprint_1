a348e0e504a3e3016504551f1afedf6dea4b8802
aigt <ai-kr@yandex.ru>
2022-08-13 17:07:29 +0500

    Константа DEFAULT_AUTO_FIELD перенесена из общих в настройки базы данных

--- movies_admin/config/settings/components/common.py
+++ movies_admin/config/settings/components/common.py
@@ -76,8 +76,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
