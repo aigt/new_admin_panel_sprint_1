2a8743181a82a30ba4de46eb522eec3230364d82
aigt <ai-kr@yandex.ru>
2022-08-19 23:36:01 +0500

    Создан писатель для таблицы кинопроизведений

--- sqlite_to_postgres/tools/select_table.py
+++ sqlite_to_postgres/tools/select_table.py
@@ -5,10 +5,11 @@ import uuid
 import aiosqlite
 
 from sqlite_to_postgres import (  # isort:skip
-    pg_conn_context,
     reader,
     settings,
     sqlite_conn_context,
+    writer,
+    models,
 )
 
 ROWS_PER_READ = 20
@@ -39,28 +40,19 @@ async def show2():
 async def insert_values():
     """Записать в таблицу тестовые данные."""
     dbs = settings.DATABASES['pg']
-    async with pg_conn_context.conn_context(dbs) as conn:
-        await conn.execute(
-            """INSERT INTO film_work(
-                id,
-                title,
-                description,
-                creation_date,
-                rating,
-                type,
-                created,
-                modified
-            )
-            VALUES($1, $2, $3, $4, $5, $6, $7, $8);""",
-            str(uuid.uuid4()),
-            'Название1',
-            'Описание2',
-            datetime.datetime.today(),
-            5.6,  # noqa: WPS432
-            'tv_show',
-            datetime.datetime.utcnow(),
-            datetime.datetime.utcnow(),
-        )
+    model = models.Filmwork(
+        id=str(uuid.uuid4()),
+        title='Название1',
+        description='Описание2',
+        creation_date=datetime.datetime.today(),
+        rating=5.6,  # noqa: WPS432
+        type='tv_show',
+        file_path='',
+        created_at=datetime.datetime.utcnow(),
+        updated_at=datetime.datetime.utcnow(),
+    )
+    db_writer = writer.FilmworkWriter(dbs)
+    await db_writer.write([model])
 
 
 if __name__ == '__main__':
--- sqlite_to_postgres/writer.py
+++ sqlite_to_postgres/writer.py
@@ -1,14 +1,90 @@
 import abc
 from typing import Any
 
+import asyncpg
+
+from sqlite_to_postgres import models, pg_conn_context
+
 
 class Writer(abc.ABC):
     """Абстрактный класс-писатель в таблицу БД."""
 
-    @abc.abstractmethod
+    def __init__(self, db_settings: dict) -> None:
+        """Конструктор.
+
+        Args:
+            db_settings (dict): Настройки БД
+        """
+        super().__init__()
+        self.__db_settings = db_settings
+
     async def write(self, data_pack: list[Any]) -> None:
         """Писать набор строк в БД.
 
         Args:
             data_pack: набор строк для записи
         """
+        async with pg_conn_context.conn_context(self._db_settings) as conn:
+            await self._execute_query(
+                conn,
+                data_pack,
+            )
+
+    @abc.abstractmethod
+    async def _execute_query(
+        self,
+        conn: asyncpg.Connection,
+        data_pack: list[Any],
+    ) -> None:
+        """Запустить запись.
+
+        Args:
+            conn (asyncpg.Connection): Соединение с БД
+            data_pack (list[Any]):  набор строк для записи
+        """
+
+    @property
+    def _db_settings(self) -> dict:
+        return self.__db_settings
+
+
+class FilmworkWriter(Writer):
+    """Класс-писатель для записи кинопроизведений в таблицу БД."""
+
+    async def _execute_query(
+        self,
+        conn: asyncpg.Connection,
+        data_pack: list[models.Filmwork],
+    ) -> None:
+        """Запустить запись.
+
+        Args:
+            conn (asyncpg.Connection): Соединение с БД
+            data_pack (list[Any]):  набор строк для записи
+        """
+        await conn.executemany(
+            """INSERT INTO film_work(
+                id,
+                title,
+                description,
+                creation_date,
+                rating,
+                type,
+                created,
+                modified
+            )
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8);""",
+            list(map(self._model_as_tuple, data_pack)),
+        )
+
+    def _model_as_tuple(self, model: models.Filmwork):
+        return (
+            model.id,
+            model.title,
+            model.description,
+            model.creation_date,
+            model.rating,
+            model.type,
+            model.created_at,
+            model.updated_at,
+        )
