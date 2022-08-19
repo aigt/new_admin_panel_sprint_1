983bf408ac7fadf6968fc5290ac4bd36ca94a24b
aigt <ai-kr@yandex.ru>
2022-08-19 21:47:22 +0500

    Рефакторин ридера
    db_path перенесён в абстракный класс
    Заданы области видимости для всех атрибутов

--- sqlite_to_postgres/reader.py
+++ sqlite_to_postgres/reader.py
@@ -10,17 +10,54 @@ from sqlite_to_postgres import models, sqlite_conn_context
 class Reader(abc.ABC):
     """Абстрактный класс-читатель таблицы из БД."""
 
-    def __init__(self, *, size=1) -> None:
+    def __init__(self, db_path: str, *, size=1) -> None:
         """Конструктор.
 
         Args:
-            size (int, optional): Количество считываемых рядов за раз.
-                Defaults to 1.
+            db_path (str): Путь к БД.
+            size (int, optional): Количество считываемых рядов за раз. По
+                                  умолчанию = 1.
         """
-        self.size = size
+        self.__db_path = db_path  # noqa: WPS112
+        self._size = size
+
+    async def read(self) -> list[Any]:
+        """Читать набор строк из БД (количество определено в свойстве size).
+
+        Yields:
+            list[Any]: считанный набор строк
+        """
+        async with sqlite_conn_context.conn_context(self._db_path) as conn:
+            conn.row_factory = self._row_factory
+            async with conn.execute(self._fetch_query) as curs:
+                curs.arraysize = int(self._size)
+                selected_data = await curs.fetchmany()
+                while selected_data:
+                    yield selected_data
+                    selected_data = await curs.fetchmany()
+
+    def _build_attrs(
+        self,
+        cursor: aiosqlite.Cursor,
+        row: tuple[Any],
+    ) -> dict:
+        """Фабрика преобразующая строки таблиц в словарь.
+
+        Args:
+            cursor (aiosqlite.Cursor): курсор базы данных
+            row (tuple[Any]): строка таблицы БД
+
+        Returns:
+            dict: словарь с атрибутами для создания класса
+        """
+        col_names = tuple(
+            map(itemgetter(0), cursor.description),
+        )
+
+        return {key: col_val for key, col_val in zip(col_names, row)}
 
     @abc.abstractmethod
-    def row_factory(self, cursor: aiosqlite.Cursor, row: tuple[Any]) -> Any:
+    def _row_factory(self, cursor: aiosqlite.Cursor, row: tuple[Any]) -> Any:
         """Фабрика преобразующая строки таблиц в датакласс.
 
         Args:
@@ -31,23 +68,13 @@ class Reader(abc.ABC):
             Any: датакласс
         """
 
-    async def read(self) -> list[Any]:
-        """Читать набор строк из БД (количество определено в свойстве size).
-
-        Yields:
-            list[Any]: считанный набор строк
-        """
-        async with sqlite_conn_context.conn_context(self.db_path) as conn:
-            conn.row_factory = self.row_factory
-            async with conn.execute(self.fetch_query) as curs:
-                curs.arraysize = int(self.size)
-                selected_data = await curs.fetchmany()
-                while selected_data:
-                    yield selected_data
-                    selected_data = await curs.fetchmany(self.size)
+    @property
+    @abc.abstractmethod
+    def _fetch_query(self) -> str:
+        """Запрос к БД."""
 
     @property
-    def size(self) -> int:
+    def _size(self) -> int:
         """Количество строк для чтения за раз.
 
         Returns:
@@ -55,8 +82,8 @@ class Reader(abc.ABC):
         """
         return self.__size
 
-    @size.setter
-    def size(self, size_value: int):
+    @_size.setter
+    def _size(self, size_value: int):
         """Сеттер количества строк для чтения за раз.
 
         Args:
@@ -65,14 +92,13 @@ class Reader(abc.ABC):
         self.__size = size_value  # noqa: WPS112
 
     @property
-    @abc.abstractmethod
-    def db_path(self) -> str:
-        """Путь к базе данных."""
+    def _db_path(self) -> str:
+        """Путь к БД.
 
-    @property
-    @abc.abstractmethod
-    def fetch_query(self) -> str:
-        """Запрос к БД."""
+        Returns:
+            str: Путь к БД
+        """
+        return self.__db_path
 
 
 class FilmworkReader(Reader):
@@ -83,12 +109,11 @@ class FilmworkReader(Reader):
 
         Args:
             db_path (str): Путь к БД.
-            size (int): Количество считываемых рядов за раз. Defaults to 1.
+            size (int): Количество считываемых рядов за раз. По умолчанию = 1.
         """
-        super().__init__(size=size)
-        self.__db_path = db_path  # noqa: WPS112
+        super().__init__(db_path=db_path, size=size)
 
-    def row_factory(
+    def _row_factory(
         self,
         cursor: aiosqlite.Cursor,
         row: tuple[Any],
@@ -102,25 +127,11 @@ class FilmworkReader(Reader):
         Returns:
             models.Filmwork: датакласс Filmwork
         """
-        col_names = tuple(
-            map(itemgetter(0), cursor.description),
-        )
-
-        attrs = {key: col_val for key, col_val in zip(col_names, row)}
-
+        attrs = self._build_attrs(cursor, row)
         return models.Filmwork(**attrs)
 
     @property
-    def db_path(self) -> str:
-        """Путь к БД.
-
-        Returns:
-            str: Путь к БД
-        """
-        return self.__db_path
-
-    @property
-    def fetch_query(self) -> str:
+    def _fetch_query(self) -> str:
         """Запрос к БД.
 
         Returns:
