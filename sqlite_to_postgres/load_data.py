381969255dcd2f7b20bafd24a2a8630edecdbf6b
aigt <ai-kr@yandex.ru>
2022-08-20 21:23:09 +0500

    Добавлен модуль для запуска сервиса переноса данных

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,31 +1,13 @@
-import sqlite3
+import asyncio
 
-import psycopg2
-import settings
-from psycopg2.extensions import connection as _connection
-from psycopg2.extras import DictCursor
+from sqlite_to_postgres.copier import copier
+from sqlite_to_postgres.settings import settings
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres.
-
-    Args:
-        connection: sqlite3 Connection
-        pg_conn: Postgres Connection
-    """
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
-
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+async def load_from_sqlite():
+    """Основной метод загрузки данных из SQLite в Postgres."""
+    await copier.carry_over(settings.JOBS)
 
 
 if __name__ == '__main__':
-    dsl = settings.DATABASES['pg']
-    sqlite_db_path = settings.DATABASES['sqlite']['db_path']
-
-    with (
-        sqlite3.connect(sqlite_db_path) as sqlite_conn,
-        psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn,
-    ):
-        load_from_sqlite(sqlite_conn, pg_conn)
+    asyncio.run(load_from_sqlite())
